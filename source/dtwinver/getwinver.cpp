6fb1a8cdc81a68fe99e5c70a283fb890c60e8f72
Dynarithmic <paulm@dynarithmic.com>
2020-03-15 05:33:01 -0400

    Remove LPGL license text from source.

--- source/dtwinver/getwinver.cpp
+++ source/dtwinver/getwinver.cpp
@@ -2,17 +2,21 @@
     This file is part of the Dynarithmic TWAIN Library (DTWAIN).
     Copyright (c) 2002-2020 Dynarithmic Software.
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License version 3 (AGPL)
-    as published by the Free Software Foundation with the addition of the
-    following permission added to Section 15 as permitted in Section 7(a):
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
     FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
     DYNARITHMIC SOFTWARE. DYNARITHMIC SOFTWARE DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
     OF THIRD PARTY RIGHTS.
-
-    For more information, the license file named LICENSE that is located in the root
-    directory of the DTWAIN installation covers the restrictions under the AGPL license.
-    Please read this file before deploying or distributing any application using DTWAIN.
  */
 #ifdef _WIN32
 #include "windows_info.inl"
