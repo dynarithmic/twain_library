335eae5ce2e0dee745ee6cc1125dbc88fbba19d1
Dynarithmic <paulm@dynarithmic.com>
2020-12-28 22:26:55 -0500

    Add C and C++ examples.

---
+++ language_bindings_and_examples/c_cpp/SimpleConsole/SimpleConsole.h
@@ -0,0 +1,26 @@
+#ifndef EXAMPLE1_H
+#define EXAMPLE1_H
+
+#define IDC_MYICON                      2
+#define IDD_EXAMPLE1_DIALOG             102
+#define IDD_ABOUTBOX                    103
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDS_HELLO                       106
+#define IDI_EXAMPLE1                    107
+#define IDI_SMALL                       108
+#define IDC_EXAMPLE1                    109
+#define IDR_MAINFRAME                   128
+#define IDM_RETRIEVE_DIB                32773
+#define IDC_STATIC                      -1
+
+#define IDD_DIALOG1                     1000
+#define IDC_frmImage                    1001
+#define IDC_btnPrev                     1002
+#define IDC_btnNext                     1003
+#define IDC_edCurPage                   1004
+#define IDC_edNumPages                  1005
+#define IDC_cmbAcquisition              1006
+#endif
+
