9a52302a7dcbae9546fa7caf770716d3bd61505a
Dynarithmic <paulm@dynarithmic.com>
2025-11-21 14:52:08 -0500

    Update master-staging to DTWAIN version 5.7.1 Build 016

--- demos/csharp/dtwain32ud.cs
+++ demos/csharp/dtwain32ud.cs
@@ -2,7 +2,7 @@ using System.Runtime.InteropServices;
 
 /*
     This file is part of the Dynarithmic TWAIN Library (DTWAIN).
-    Copyright (c) 2002-2025 Dynarithmic Software.
+    Copyright (c) 2002-2026 Dynarithmic Software.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
